ac30dd457fc95aa6a657e2b315c43a8fec2e1eaa
Nigel Jacob <nigeljacob@Nigels-MacBook-Air.local>
2024-01-31 11:30:34 +0530

    main Dashboard

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,29 @@
 import "./App.css";
 import SideBar from "./components/SideBar";
 import { BrowserRouter as Router, Routes as Switch, Route } from "react-router-dom";
+import Activity from "./Pages/Activity"
+import Chat from "./Pages/Chats";
+import Teams from "./Pages/Teams";
+import * as IOIcons from "react-icons/io5"
 
 function App() {
   return (
     <>
+      <div className="titleBar">
+      </div>
+      <div className="main">
       <Router>
         <SideBar />
         <Switch>
-          <Route path="/" />
+          <Route path="/" exact Component={Activity}/>
+          <Route path="/Chats" Component={Chat}/>
+          <Route path="/Teams" Component={Teams}/>
         </Switch>
       </Router>
+      <div className="settingsButton">
+          <IOIcons.IoSettings className="SettingIcon" />
+      </div>
+      </div>
     </>
   );
 }
---
+++ src/Pages/Chats.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Chats() {
+  return (
+    <div className='chat'>
+      <h1>Chats</h1>
+    </div>
+  )
+}
+
+export default Chats;
