805cba5565837a26ffbf0a3d9374c14eb084d6d6
Sevinda Perera <sevindacharukaperera@gmail.com>
2024-02-06 19:16:29 +0530

    Dynamic Task Details

---
+++ src/components/TaskDetails.jsx
@@ -0,0 +1,31 @@
+import "../Pages/TeamMemberDashboard.css";
+
+const TaskDetails = ({ index, taskDesc, taskStatus }) => {
+  return (
+    <div className="single-task-container">
+      <p>{index + ". " + taskDesc}</p>
+
+      <div className="status-container">
+        <select
+          name="status"
+          className={
+            taskStatus === "Completed" ? "green-status" : "yellow-status"
+          }
+        >
+          <option value="start" disabled selected hidden>
+            {taskStatus}
+          </option>
+          <option value="inProgress">In Progress</option>
+          <option value="completed">Completed</option>
+        </select>
+        {taskStatus !== "Completed" ? (
+          <div className="status">In Progress</div>
+        ) : (
+          <div className="status">Completed</div>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default TaskDetails;
