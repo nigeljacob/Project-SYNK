eb0f45aa865d89f17b9137d86f50cd5a57551062
Nigel Jacob <nigeljacob@Nigels-MacBook-Air.local>
2024-02-19 22:12:25 +0530

    login complete

---
+++ Backend/src/classes.js
@@ -0,0 +1,36 @@
+
+export const Team = (teamCode, teamName, teamProfile, teamDescription, teamLeader, teamMemberList, projectLength, taskList, teamStatus) => {
+    return {
+        "teamCode": teamCode,
+        "teamName": teamName,
+        "teamProfile": teamProfile,
+        "teamDescription": teamDescription,
+        "teamLeader": teamLeader,
+        "teamMemberList": teamMemberList,
+        "projectLength": projectLength,
+        "taskList": taskList,
+        "teamStatus": teamStatus
+    }
+}
+
+export const User = () => {
+    return {
+
+    }
+}
+
+export const Task = () => {
+    return {
+        
+    }
+}
+
+export const Message = () => {
+    
+}
+
+export const Progress = () => {
+    return {
+        
+    }
+}
