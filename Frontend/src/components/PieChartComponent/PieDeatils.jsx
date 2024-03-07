e75bf8c18d965b0384cca0f55aa13309b43beb60
Sasri17 <sasri.2022017@iit.ac.lk>
2024-03-07 13:20:49 +0530

    recorrection

--- Frontend/src/components/PieChartComponent/PieDeatils.jsx
+++ Frontend/src/components/PieChartComponent/PieDeatils.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
-var a = "60"
+var totalTime = "60"
+var taskCompletedDate  = "03/07/2024"
+var taskAssigneddDate = "03/18/2024"
+
+
+
 function PieDetails () {
     return(
         <div className=""> 
-          <h1 className="tw-ml-20 tw-font-semibold">Total of {a} Miniutes Spent on This Task</h1>
+          <h1 className="tw-ml-20 tw-font-semibold">Total of {totalTime} Miniutes Spent on This Task</h1>
           <ul className="tw-ml-20 tw-mt-[20px]">
-            <li className="tw-mt-10 tw-list-disc tw-ml-5 tw-text-lg">Task Assigned - 2022/03/12</li>
-            <li className="tw-mt-10 tw-list-disc tw-ml-5 tw-text-lg">Task Completed - 2023/03/18</li>
-            <li className="tw-mt-10 tw-list-disc tw-ml-5 tw-text-lg">Task Assigned - 2024/03/18</li>
+            <li className="tw-mt-10 tw-list-disc tw-ml-5 tw-text-lg">Task Assigned -{taskCompletedDate}</li>
+            <li className="tw-mt-10 tw-list-disc tw-ml-5 tw-text-lg">Task Completed -{taskAssigneddDate} </li>
             <li className="tw-mt-10 tw-text-green-500 tw-font-semibold tw-text-lg">Task Completed 2 Days Before Deadline</li>
           </ul>
 
