b94b1f26e6d8a30730d5890a42a446c9e92bf9bf
Sasri17 <sasri.2022017@iit.ac.lk>
2024-03-07 12:46:09 +0530

    json file added

--- Frontend/src/components/PieChartComponent/PieChart.jsx
+++ Frontend/src/components/PieChartComponent/PieChart.jsx
@@ -1,29 +1,22 @@
 import React from "react";
 import {Chart as ChartJS} from "chart.js/auto";
 import { Doughnut } from "react-chartjs-2";
+import PieData from "./PieData/PieData.json"
 
 const PieChart = () => {
     // Dummy data
     const data = {
-        labels: ["Ms Word", "Chrome", "Git",],
+        labels: PieData.map((data) => data.label),
         datasets: [
             {
-                label: "Total Miniutes",
-                data: [20, 70, 20],
-                backgroundColor: [
-                    "rgb(116,238,21)",
-                    "rgb(77,238,234)",
-                    "rgb(254,0,0)",
-                ],
-                borderColor: [
-                    "rgb(116,238,21)",
-                    "rgb(77,238,234)",
-                    "rgb(254,0,0)",
-                ],
-                borderWidth: 1,
+                label: "Count",
+                data:PieData.map((data) => data.value),
             },
+
         ],
     };
+       
+    
 
     return (
         <div className="tw-h-[350px] ">
