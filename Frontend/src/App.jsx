9f30d52c8b6f08ec0d95afb74f7041e1b5289fe0
MilzOverkill <nanayakkaramilni@gmail.com>
2024-03-15 11:57:04 +0530

    Implemented automatic version upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -230,8 +230,7 @@ function App() {
   useEffect(() => {
     electronApi.receiveUrlFromMain((data) => {
         let URL = data.URL
-        console.log(URL)
-        uploadVersion(URL, data.task, data.index, data.team, data.teamMemberIndex).then(() => {
+        uploadVersion(URL, data.task, data.taskIndex, data.team, data.teamMemberIndex).then(() => {
           electronApi.sendUploadVersion(data)
         })
     })
